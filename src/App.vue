812b3c2331c9b02764d24bbff7cbd32fb6bded75
TiagoSpada <tiago.spada.ts@gmail.com>
2024-05-29 20:15:39 +0200

    added AppTitle

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
-<script></script>
-<template></template>
+<script>
+import AppTitle from "./components/AppTitle.vue";
+export default {
+	name: "myApp",
+	components: {
+		AppTitle,
+	},
+};
+</script>
+<template>
+	<AppTitle />
+</template>
 <style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+	name: "AppTitle",
+	data() {
+		return {
+			title: "La mia prima app con Vite!",
+		};
+	},
+};
+</script>
+<template>
+	<h1>{{ title }}</h1>
+</template>
+<style>
+h1 {
+	text-align: center;
+	margin: auto;
+	margin-top: 100px;
+	max-width: 25%;
+	border: 2px solid black;
+	padding: 5px;
+	border-radius: 10px;
+}
+</style>
